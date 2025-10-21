73ce161f18b5768d545e281450f138a6dc62b93d
Miko Ofiaza <157573595+MikoOfiaza@users.noreply.github.com>
2025-10-21 23:33:13 +0800

    Added "Add to Cart" button

--- app/Food/index.tsx
+++ app/Food/index.tsx
@@ -1,24 +1,23 @@
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
- const variantImages = {
-    "kawasaki aurora 7 light": require("./../../images/aurora7.jpeg"),
-    "kawasaki baghonor s6": require("./../../images/baghonors6.jpg"),
-    "kawasaki master 900": require("./../../images/master900.jpg"),
-    "kawasaki 900": require("./../../images/shoes.jpeg"),
-    "kawasaki traverse": require("./../../images/traverse.webp"),
-    "kawasaki schafer": require("./../../images/schafer.jpg"),
-    "kawasaki ksb 90": require("./../../images/string2.jpeg"),
-    "kawasaki elite 65": require("./../../images/elite65.jpeg"),
-    "kawasaki ksb 80": require("./../../images/ksb80.webp"),
-    "kawasaki k1f00": require("./../../images/k1f00.webp"),
-    "kawasaki bs334": require("./../../images/bs334.jpg"),
-    "kawasaki 51045": require("./../../images/51045.jpg"),
-  };
+const variantImages = {
+  "kawasaki aurora 7 light": require("./../../images/aurora7.jpeg"),
+  "kawasaki baghonor s6": require("./../../images/baghonors6.jpg"),
+  "kawasaki master 900": require("./../../images/master900.jpg"),
+  "kawasaki 900": require("./../../images/shoes.jpeg"),
+  "kawasaki traverse": require("./../../images/traverse.webp"),
+  "kawasaki schafer": require("./../../images/schafer.jpg"),
+  "kawasaki ksb 90": require("./../../images/string2.jpeg"),
+  "kawasaki elite 65": require("./../../images/elite65.jpeg"),
+  "kawasaki ksb 80": require("./../../images/ksb80.webp"),
+  "kawasaki k1f00": require("./../../images/k1f00.webp"),
+  "kawasaki bs334": require("./../../images/bs334.jpg"),
+  "kawasaki 51045": require("./../../images/51045.jpg"),
+};
 
 export default function FoodDetails() {
   const { productId, variantName } = useLocalSearchParams();
-  
 
   // Get the image based on variantName
   const imageSource = variantImages[String(variantName).toLowerCase()];
@@ -40,6 +39,8 @@ export default function FoodDetails() {
       {imageSource && (
         <Image source={imageSource} style={styles.image} resizeMode="cover" />
       )}
+
+      <button style={styles.addToCartButton}>Add to cart</button>
     </View>
   );
 }
@@ -83,5 +84,16 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     marginTop: 20,
     backgroundColor: "#eee",
+  },
+  addToCartButton: {
+    width: 200,
+    padding: 15,
+    backgroundColor: "#E63946",
+    borderRadius: 10,
+    color: "#fff",
+    marginTop: 30,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
